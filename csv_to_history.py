2069a9fa2dfd211ce2a4f9b2005a978c73b6a6f9
jihanmsr <222112122@stis.ac.id>
2026-08-21 13:35:59 +0800

    Update progress petugas 21 Agustus

--- csv_to_history.py
+++ csv_to_history.py
@@ -48,4 +48,4 @@ def process_csv_files(csv_pattern, date_str):
             ])
     print(f"Generated {csv_file}")
 
-process_csv_files('/Users/jihanmaisaroh/scrap_fasih/progres_20/sqllab_rekap_progress_petugas_20260820T*.csv', '2026-08-20')
+process_csv_files('/Users/jihanmaisaroh/scrap_fasih/progres_21/sqllab_rekap_progress_petugas_20260821T*.csv', '2026-08-21')
