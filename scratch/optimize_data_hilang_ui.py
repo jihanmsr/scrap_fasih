b6a53cbb35df26b3bf32c3aae02302e25dc678c2
jihanmsr <222112122@stis.ac.id>
2026-08-29 16:39:01 +0800

    chore: clean up unused test files, temporary browser profiles, and obsolete dumps

--- scratch/optimize_data_hilang_ui.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('/Users/jihanmaisaroh/scrap_fasih/index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-# 1. Update title and description in the UI
-# The user wants dynamic title depending on the tab, but for now we can update the static header in HTML.
-# But wait, the header is defined in index.html in the <main> section somewhere?
-# No, the screenshot shows "Pemantauan Data Hilang". That's probably in the <h2> of the section.
--- test_script.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const js = fs.readFileSync('/Users/jihanmaisaroh/scrap_fasih/open_subsls.js', 'utf8');
-try {
-  new Function(js);
-  console.log("Syntax is valid");
-} catch(e) {
-  console.log("Error:", e);
-}
